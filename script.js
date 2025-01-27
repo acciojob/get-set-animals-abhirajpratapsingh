e1b0f1a7c6dadad32560193bcb3e6e94ae807fc5
abhiraj pratap singh <112178869+abhirajpratapsingh@users.noreply.github.com>
2025-01-27 17:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-// Define the Animal class
 class Animal {
     constructor(species) {
-        this._species = species; // Private variable for species
+        this._species = species;
     }
 
-    // Getter for species
     get species() {
         return this._species;
     }
 
-    // Method to log the sound the animal makes
     makeSound() {
         console.log(`The ${this.species} makes a sound`);
     }
 }
 
-// Define the Cat class that extends Animal
 class Cat extends Animal {
     purr() {
         console.log("purr");
     }
 }
 
-// Define the Dog class that extends Animal
 class Dog extends Animal {
     bark() {
         console.log("woof");
     }
 }
-
